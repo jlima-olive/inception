31dc7d2d678617f6995c1f9a009c7faea0400292
namejojo <jlima-olive@gmail.com>
2026-04-08 14:01:39 +0000

    update

--- Makefile
+++ Makefile
@@ -5,11 +5,15 @@ LOGIN = $${USER}
 DATA_PATH = /home/$(LOGIN)/data
 DB_PATH = $(DATA_PATH)/mariadb
 WP_PATH = $(DATA_PATH)/wordpress
+DOMAIN = $$(cat .env | grep DOMAIN_NAME | awk -F= '{printf $$2}')
 
 all: up
 
-up: create_dirs
+up: host create_dirs
 	$(COMPOSE) up --build
+
+host:
+	sudo sh -c "echo 127.0.0.1 $(DOMAIN) www.$(DOMAIN) >> /etc/hosts"
 
 down:
 	$(COMPOSE) down
@@ -36,11 +40,11 @@ create_dirs:
 	mkdir -p $(WP_PATH)
 
 fclean: down
-	docker stop $$(docker ps | awk 'NR>1 {printf $1 " "}') || true
+	docker stop $$(docker ps | awk 'NR>1 {printf $$1 " "}') || true
 	sudo rm -rf $(DB_PATH)/*
 	sudo rm -rf $(WP_PATH)/*
 	docker system prune -a -f --volumes
 
 re: fclean up
 
-.PHONY: all up down start stop restart build logs ps create_dirs fclean re
+.PHONY: all up down start stop restart build logs ps create_dirs fclean re host
--- wordpress/init_wp.sh
+++ wordpress/init_wp.sh
@@ -4,33 +4,25 @@ set -e
 
 until mariadb -u"$MYSQL_USER" -p"$MYSQL_PASSWORD" "$MYSQL_DATABASE" -e "SELECT 1;" >/dev/null 2>&1; do
 	echo "Waiting for MariaDB..."
-	sleep 2
+	sleep 1
 done
 
 if [ ! -f "/var/www/html/wp-config.php" ]; then
 	echo "Downloading WordPress..."
 
-	cd /tmp
 	wget https://wordpress.org/latest.tar.gz
-
 	tar -xzf latest.tar.gz
-	cp -a wordpress/. /var/www/html/
+	mv -a wordpress/. /var/www/html/
+	rm -rf /tmp/latest.tar.gz
 
-	# Remove temporary installation files
-	rm -rf /tmp/latest.tar.gz /tmp/wordpress
-
-	# Move to the WordPress directory to run WP-CLI commands
 	cd /var/www/html
 
-	# Generate the wp-config.php file with database connection settings
 	wp config create \
 		--allow-root \
 		--dbname="$MYSQL_DATABASE" \
 		--dbuser="$MYSQL_USER" \
 		--dbpass="$MYSQL_PASSWORD" \
 		--dbhost="$MYSQL_HOST:3306" 
-	
-	# Install WordPress core and create the administrator account
 	wp core install \
 		--allow-root \
 		--url="$DOMAIN_NAME" \
@@ -39,8 +31,6 @@ if [ ! -f "/var/www/html/wp-config.php" ]; then
 		--admin_password="$WP_ADMIN_PASSWORD" \
 		--admin_email="$WP_ADMIN_EMAIL" \
 		--skip-email
-
-	# Create an additional WordPress user required by the subject
 	wp user create "$WP_USER" "$WP_USER_EMAIL" \
 		--allow-root \
 		--role=author \
@@ -51,6 +41,4 @@ fi
 
 echo "Starting WordPress..."
 
-# Replace the shell with php-fpm and keep it in the foreground
-# The -F option prevents php-fpm from running as a daemon, which is required in Docker
 exec /usr/sbin/php-fpm8.2 -F
